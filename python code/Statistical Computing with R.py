96f7bc730a30b9d77bb939f1932bdd8b23cc7836
xanalee <32980222+xanalee@users.noreply.github.com>
2023-10-31 01:04:34 +0100

    2023-10-31 Tuesday push4
    1. Initial commit

---
+++ python code/Statistical Computing with R.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@Time ：2023/10/18 11:41 
+@Auth ：xana lee
+@File ：Statistical Computing with R.py
+@IDE ：PyCharm
+@Motto ：ABC(Always Be Coding)
+"""
---
+++ python code/Statistics and Probability.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@Time ：2023/10/18 11:41 
+@Auth ：xana lee
+@File ：Statistics and Probability.py
+@IDE ：PyCharm
+@Motto ：ABC(Always Be Coding)
+"""
